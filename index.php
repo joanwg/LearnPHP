9c47e50f3fde10559c66dd4339e0b9d981e9342e
Angie Wong <joanwghk@yahoo.com.hk>
2015-07-18 21:55:27 +0800

    Ex 1&2 --> DONE

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
+<?php
 
+//Exercise 1
+
+/*hi
+ I am Angie!*/
+
+//I echo Hello world to the browser
+echo "Hello world!";
+
+?>
+
+<?php
+
+//Exercise 2
+
+$myName123 = "Angie Wong";
+$luckyNumber = 9*2/1;
+$number2 = 3;
+
+echo $myName123." is me, ".$luckyNumber-$number2." is not my lucky number.";
+/*Angie Wong is me, 15 is not my lucky number.*/
+
+//Gordon Mak is a boy.  His age is (15-2+1)!
+$name = "Gordon Mak";
+$gender = "boy";
+$var1 = 15;
+$var2 = 2;
+$var3 = 1;
+$ans = $var1-$var2+$var3;
+
+echo $name." is a ".$gender.".  His age is ". $ans."!";
+
+?>
